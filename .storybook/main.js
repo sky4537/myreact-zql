94f4b7ec7cd48f67d4a04b37a5458429554310d1
赵钱龙 <qianlong.zhao@china.zhaogang.com>
2020-08-20 19:42:58 +0800

    test

---
+++ .storybook/main.js
@@ -0,0 +1,40 @@
+module.exports = {
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/preset-create-react-app",
+    "@storybook/addon-actions",
+		"@storybook/addon-viewport",
+		"@storybook/addon-knobs",
+		"@storybook/addon-a11y",
+    "@storybook/addon-storysource",
+    { name: "@storybook/addon-docs", options: { configureJSX: true } }
+  ],
+  webpackFinal: async (config) => {
+		config.module.rules.push({
+			test: /\.(ts|tsx)$/,
+			use: [
+				{
+          loader: require.resolve("react-docgen-typescript-loader"),
+          options: {
+						shouldExtractLiteralValuesFromEnum: true,
+						propFilter: (prop) => {
+							if (prop.parent) {
+								return !prop.parent.fileName.includes(
+									"node_modules"
+								);
+							}
+							return true;
+						},
+					},
+				},
+			],
+		});
+		config.resolve.extensions.push(".ts", ".tsx");
+		return config;
+	},
+}
