0f1baf02f08aae51123d6972341ce41b84f62631
sky4537 <48868041@qq.com>
2020-08-21 09:27:43 +0000

    Deploying to gh-pages from  @ a29b994aa2feac350429303f336f72e056317494 🚀

---
+++ precache-manifest.54b2bcdb5e8842e981f8c729df2b89cf.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d658380beba56ab405955bfcce43a839",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.0e317f778342df99ff3a.bundle.js"
+  },
+  {
+    "url": "runtime~main.0e317f778342df99ff3a.bundle.js"
+  },
+  {
+    "url": "vendors~main.0e317f778342df99ff3a.bundle.js"
+  },
+  {
+    "url": "vendors~main.0e317f778342df99ff3a.bundle.js.LICENSE.txt"
+  }
+]);
